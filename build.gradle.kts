4b4ee19fc15fc11bd0756d9eea6cce5f87cad8fd
Lukas Forst <lukas@forst.pw>
2020-03-06 13:19:10 +0100

    add lithium dependency and cryptobox transitive dependency fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,27 @@
 plugins {
     java
+    `java-library`
 }
 
 group = "com.wire.bots.simulation"
 version = "0.0.1"
 
 repositories {
-    mavenCentral()
+    jcenter()
+
+    // lithium
+    maven {
+        url = uri("https://packagecloud.io/dkovacevic/lithium/maven2")
+    }
+
+    // transitive dependency for the lithium
+    maven {
+        url = uri("https://packagecloud.io/dkovacevic/cryptobox4j/maven2")
+    }
 }
 
 dependencies {
+    api("com.wire.bots", "lithium", "2.34.5")
     testImplementation("junit", "junit", "4.12")
 }
 
